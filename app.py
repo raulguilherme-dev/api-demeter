c5bfadf8266925dd1fa31a5da2b9c2b021d0e6c1
raul <raul.guilherme075@gmail.com>
2023-04-27 17:29:13 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,123 @@
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import JSON
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='secret'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_HOST')
+
+db=SQLAlchemy(app)
+
+class Requisicao(db.Model):
+    _tablename_= "requisicao"
+
+    id_req = db.Column(db.Integer, primary_key=True)
+    valor = db.Column(db.Float)
+
+class UltimaRequisicao(db.Model):
+    _tablename_ = "ultima_requisicao"
+
+    id_last_req = db.Column(db.Integer, primary_key=True)
+    last_req = db.Column(db.Integer)
+
+class Total(db.Model):
+    _tablename_ = "total"
+    
+    id_total = db.Column(db.Integer, primary_key=True)
+    total = db.Column(db.Float)
+
+class Clima(db.Model):
+    _tablename_ = "clima"
+
+    id_clima = db.Column(db.Integer, primary_key=True)
+    temperatura = db.Column(db.Float)
+    umidade = db.Column(db.Integer)
+    horario = db.Column(db.DateTime)
+
+    def __init__(self, temperatura, umidade, horario):
+        self.temperatura = temperatura
+        self.umidade = umidade
+        self.horario = horario
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET'])
+def teste():
+    return 'OI'
+
+@app.route('/req', methods=['GET', 'POST'])
+def get():
+    if request.method == "POST":
+        try: 
+            json = request.get_json()
+            valor = Requisicao(valor=float(json['valor']))
+            db.session.add(valor)
+            db.session.commit()
+            return jsonify({'message': 'Enviado com sucesso'})
+        
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return jsonify({'message' : 'Falha ao enviar'})
+
+    if request.method == "GET":
+        obj = Requisicao.query.order_by(Requisicao.id_req.desc()).limit(1).first()
+        resposta = jsonify({"id_valor": obj.id_req, "valor": obj.valor})
+        return resposta
+
+@app.route('/last-req', methods=['POST', 'GET'])
+def req():
+    if request.method == "POST":
+        json = request.get_json()
+        last_req = UltimaRequisicao(last_req=int(json['last_req']))
+        db.session.add(last_req)
+        db.session.commit()
+        return jsonify({"last_req": last_req.last_req})
+    
+    if request.method == "GET":
+        resposta = UltimaRequisicao.query.order_by(UltimaRequisicao.id_last_req.desc()).limit(1).first()
+        return jsonify({"last_req": resposta.last_req})
+
+@app.route('/total', methods=['POST'])
+def total():
+    if request.method == "POST":
+        json = request.get_json()
+        total = Total(total=float(json["total"]))
+        if Total.query.first() is None:
+            db.session.add(total)
+            db.session.commit()
+            return jsonify({'total': total.total})
+        else:
+            tot_atual = Total.query.order_by(Total.id_total.desc()).first()
+            novo_total = total.total + tot_atual.total
+            db_total = Total(total=float(novo_total))
+            db.session.add(db_total)
+            db.session.commit()
+            return jsonify({'total': novo_total})
+
+@app.route('/clima', methods=['POST', 'GET'])
+def clima():
+    if request.method == "POST":
+        json = request.get_json()
+        print(json)
+        try:
+            dados = Clima(json['temperatura'], json['umidade'], datetime.now())
+            db.session.add(dados)
+            db.session.commit()
+            return jsonify({'message': 'Enviado com sucesso'})
+        
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return jsonify({'message': 'Falha ao enviar'})
+
+    if request.method == "GET":
+        return render_template('index.html', dados=Clima.query.order_by(Clima.id_clima)).limit(40).all()
+
+
+if __name__ == "main":
+    app.run(debug=True)
